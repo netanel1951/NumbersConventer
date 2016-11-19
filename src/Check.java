f6aa82f536cb8135409f968702c6bbcfda7dfaec
netanel1951 <netanelesman@gmail.com>
2016-11-19 19:14:40 +0200

    Conventer

--- src/Check.java
+++ src/Check.java
@@ -5,6 +5,8 @@ public class Check
 	public static void main(String[]args) {
 		Scanner reader = new Scanner(System.in);
 		String a = reader.next();
+		a = Conventer.toDec(a, 16);
+		System.out.println(a);
 		System.out.println(Conventer.decTo(a,16));
 	}
 }
--- src/Conventer.java
+++ src/Conventer.java
@@ -2,17 +2,43 @@
 public class Conventer
 {
 	public static String decTo(String dec, int base) {
+		try{
+			int temp = Integer.parseInt(dec);
+			if(temp == 0) {
+				return "0";
+			}
+			char[] numbers = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+			String result = "";
+			while(temp != 0){
+				result = numbers[temp%base] + result;
+				temp /= base;
+			}
+			return result;
+		}
+		catch(NumberFormatException e) {
+			return "error";
+		}
+
+	}
+	public static String toDec(String num, int base) {
 		char[] numbers = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-		int temp = Integer.parseInt(dec);
-		if(temp == 0) {
-			return "0";
+		int result = 0;
+		int multi = 1;
+		boolean legal = false;
+		for(int i = num.length()-1; i>=0; i--) {
+			legal = false;
+			for (int t = 0; t<base; t++) {
+				if (numbers[t] == (num.charAt(i))) {
+					result += t*multi;
+					multi*=base;
+					legal = true;
+					break;
+				}
+			}
+			if (!legal) {
+				return "error";
+			}
 		}
-		
-		String result = "";
-		while(temp != 0){
-			result = numbers[temp%base] + result;
-			temp /= base;
-		}
-		return result;
+		return Integer.toString(result);
 	}
 }
