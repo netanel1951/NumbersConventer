558faf6234562a4314113934678c2fd057369c04
netanel1951 <netanelesman@gmail.com>
2016-11-20 14:23:09 +0200

    Final

--- src/NumbersConventer.java
+++ src/NumbersConventer.java
@@ -25,7 +25,6 @@ public class NumbersConventer extends JPanel implements ActionListener
 	JLabel num1;
 	JLabel result1;
 	int numBase = 0, resultBase = 0;
-	boolean possible1 = false, possible2 = false;
 	String[] options = {" ","binary (base 2)", "Ternary (base 3)", "Quaternary (base 4)", "Quinary (base 5)", "Senary (base 6)", "Septenary (base 7)", "Octonary (base 8)", "Nonary (base 9)", "Decimal (base 10)", "Undenary (base 11)", "Duodecimal (base 12)", "Tridecimal (base 13)", "Quattuordecimal (base 14)", "Quindecimal (base 15)", "Hexadecimal (base 16)"};
 	public NumbersConventer(){
 		frm = new JFrame ("Conventer");
@@ -89,67 +88,51 @@ public class NumbersConventer extends JPanel implements ActionListener
 			switch (String.valueOf(chs1.getSelectedItem())) {
 			case " ":
 				numBase = 0;
-				possible1 = false;
 				break;
 			case "binary (base 2)": 
 				numBase = 2;
-				possible1 = true;
 				break;
 			case "Ternary (base 3)":
 				numBase = 3;
-				possible1 = true;
 				break;
 			case "Quaternary (base 4)":
 				numBase = 4;
-				possible1 = true;
 				break;
 			case "Quinary (base 5)":
 				numBase = 5;
-				possible1 = true;
 				break;
 			case "Senary (base 6)":
 				numBase = 6;
-				possible1 = true;
 				break;
 			case "Septenary (base 7)":
 				numBase = 7;
-				possible1 = true;
 				break;
 			case "Octonary (base 8)":
 				numBase = 8;
-				possible1 = true;
 				break;
 			case "Nonary (base 9)":
 				numBase = 9;
-				possible1 = true;
 				break;
 			case "Decimal (base 10)":
 				numBase = 10;
-				possible1 = true;
 				break;
 			case "Undenary (base 11)":
 				numBase = 11;
-				possible1 = true;
 				break;
 			case "Duodecimal (base 12)":
 				numBase = 12;
-				possible1 = true;
 				break;
 			case "Tridecimal (base 13)":
 				numBase = 13;
-				possible1 = true;
 				break;
 			case "Quattuordecimal (base 14)":
 				numBase = 14;
-				possible1 = true;
 				break;
 			case "Quindecimal (base 15)":
 				numBase = 15;
-				possible1 = true;
 				break;
 			case "Hexadecimal (base 16)":
 				numBase = 16;
-				possible1 = true;
 				break;
 			}
 		}
@@ -157,73 +140,64 @@ public class NumbersConventer extends JPanel implements ActionListener
 			switch (String.valueOf(chs2.getSelectedItem())) {
 			case " ":
 				resultBase = 0;
-				possible2 = false;
 				break;
 			case "binary (base 2)": 
 				resultBase = 2;
-				possible2 = true;
 				break;
 			case "Ternary (base 3)":
 				resultBase = 3;
-				possible2 = true;
 				break;
 			case "Quaternary (base 4)":
 				resultBase = 4;
-				possible2 = true;
 				break;
 			case "Quinary (base 5)":
 				resultBase = 5;
-				possible2 = true;
 				break;
 			case "Senary (base 6)":
 				resultBase = 6;
-				possible2 = true;
 				break;
 			case "Septenary (base 7)":
 				resultBase = 7;
-				possible2 = true;
 				break;
 			case "Octonary (base 8)":
 				resultBase = 8;
-				possible2 = true;
 				break;
 			case "Nonary (base 9)":
 				resultBase = 9;
-				possible2 = true;
 				break;
 			case "Decimal (base 10)":
 				resultBase = 10;
-				possible2 = true;
 				break;
 			case "Undenary (base 11)":
 				resultBase = 11;
-				possible2 = true;
 				break;
 			case "Duodecimal (base 12)":
 				resultBase = 12;
-				possible2 = true;
 				break;
 			case "Tridecimal (base 13)":
 				resultBase = 13;
-				possible2 = true;
 				break;
 			case "Quattuordecimal (base 14)":
 				resultBase = 14;
-				possible2 = true;
 				break;
 			case "Quindecimal (base 15)":
 				resultBase = 15;
-				possible2 = true;
 				break;
 			case "Hexadecimal (base 16)":
 				resultBase = 16;
-				possible2 = true;
 				break;
 			}
 		}
 		if(src.equals(clear)) {
 			resultBase = 0;
 			numBase = 0;
+			num.setText("");
+			result.setText("");
+			chs1.setSelectedIndex(0);
+			chs2.setSelectedIndex(0);
+		}
+		if(src.equals(cnvrt) && numBase != 0 && resultBase != 0) {
+			result.setText(Conventer.decTo(Conventer.toDec(num.getText(), numBase),resultBase));
 		}
 	}
 }
